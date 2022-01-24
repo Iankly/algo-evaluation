87c7fcd38b6772e099f2939fbac2081139364885
Kylian Dantan <kyliandantan@gmail.com>
2022-01-24 15:07:16 +0000

    <votre message de commit>

--- main/01-facile.js
+++ main/01-facile.js
@@ -5,13 +5,35 @@
  * - "NUL" s'il est nul
  */
 function signe(nombre) {
-
+    if (nombre>0){
+        return 'POSITIVE'
+    }
+    else if(nombre<0){
+        return "NEGATIVE"
+    }
+    else{
+        return "NUL"
+    }
 }
 
+console.log(signe(5))
+console.log(signe(-3))
+console.log(signe(0))
 /**
  * Programmer une fonction prenant en argument un tableau de nombres, et qui renvoie un tableau ne contenant que les nombres pairs.
  * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
  */
 function nombresPairs(nombres) {
+    let a = 0;
+    let result = [];
+    while (a <= nombres.length){
+        if (nombres[a]%2 === 0){
+            result.push(nombres[a]);
+        }
+        a = a + 1
+    }
+    return result
 
 }
+
+console.log(nombresPairs([0,5,2,4,9,18,6,7,-4]))
